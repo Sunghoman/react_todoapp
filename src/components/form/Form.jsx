d3ece733f6fbdfd3d1eb8ea8b818e0b3a7501e85
kimsungho <ksh_1035@naver.com>
2022-10-03 12:45:54 +0900

    현재 입력값을 props로 하위 컴포넌트로 보내줌 + 엔터키 눌러도 포스팅 되게함

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,23 +2,38 @@ import './style.css'
 
 import List from '../list/List';
 
-const Form = ( posts ) => {
+import { useRef, useEffect } from 'react';
+
+
+
+const Form = ({ posts, setPosts, title, body, onChange, onCreate, onRemove }) => {
+
+  const titleInput = useRef(null);
+  useEffect(() => {
+      titleInput.current.focus();
+  }, []);
+
+  const onKeyPress = (e) => {
+    if(e.key === 'Enter') {
+      onCreate();
+    }
+  }
 
   return(
     <div>
       <div className='input'>
         <div className='input-post'>
           <label for="title">제목</label>
-          <input type="text" id="title" name="title"/>
+          <input type="text" ref={titleInput} id="title" name="title" onChange={onChange} value={title} onKeyPress={onKeyPress}/>
 
           <label for="comment">내용</label>
-          <input type="text" id="comment" name="body"/>
+          <input type="text" id="comment" name="body" onChange={onChange} value={body} onKeyPress={onKeyPress}/>
         </div>
-        <span className='addpost'>추가하기</span>
+        <span className='addpost' onClick={onCreate}>추가하기</span>
       </div>
-      <List posts={posts}/>
+      <List posts={posts} onRemove={onRemove} setPosts={setPosts}/>
     </div>
   );
-}
+};
 
 export default Form;
