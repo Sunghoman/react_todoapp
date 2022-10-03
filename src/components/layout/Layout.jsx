a5eca9edc6e28f85dc0e0abeb0a8e222db6db8ba
kimsungho <ksh_1035@naver.com>
2022-10-03 12:45:01 +0900

    기능구현, post기능, 삭제 기능 추가

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,12 +3,50 @@ import './style.css'
 import Header from '../header/Header';
 import Form from '../form/Form';
 
+import { useState, useRef } from 'react';
+
 const Layout = (props) => {
+
+  const [inputs, setInputs] = useState({
+    title: '',
+    body: '',
+  });
+  const { title, body } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  }
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    if (title && body) {
+      const post = {
+        id: nextId.current,
+        title,
+        body,
+        isDone: "완료!"
+      }
+      props.setPosts([...props.posts, post])
+      setInputs({
+        title: '',
+        body: ''
+      })
+      nextId.current += 1;
+    }
+  }
+
+  const onRemove = id => {
+    props.setPosts(props.posts.filter(post => post.title !== title))
+  }
 
   return(
     <div>
       <Header/>
-      <Form posts={props.posts} setPosts={props.setPosts}/>
+      <Form posts={props.posts} setPosts={props.setPosts} onChange={onChange} onCreate={onCreate} onRemove={onRemove} title={title} body={body}/>
     </div>
   );
 }
